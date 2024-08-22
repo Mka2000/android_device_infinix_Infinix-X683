59385c89631bdd5550beee369ae52a6ae121ab1d
Mka2000 <86131760+Mka2000@users.noreply.github.com>
2024-08-22 12:30:11 +0200

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_Infinix-X683.mk
+    $(LOCAL_DIR)/omni_X683.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_Infinix-X683-user \
-    omni_Infinix-X683-userdebug \
-    omni_Infinix-X683-eng
+    omni_X683-eng
